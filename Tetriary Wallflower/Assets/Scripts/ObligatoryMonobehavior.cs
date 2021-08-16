80279513e542dd7ff363911c6c27c8ff1eed0cbd
DoYouSellVHS <ghorta4@gmail.com>
2021-08-15 21:01:45 -0400

    Moved project.

---
+++ Tetriary Wallflower/Assets/Scripts/ObligatoryMonobehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObligatoryMonobehavior : MonoBehaviour
+{
+    void Start()
+    {
+        SessionManager.Initialize();
+    }
+
+    private void Update()
+    {
+        SessionManager.Update();
+    }
+}
